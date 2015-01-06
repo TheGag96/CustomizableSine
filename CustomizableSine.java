b2023f24125f3e2ded480058b8bb7b7ec5c0cae5
TheGag96 <thegag96@gmail.com>
2015-01-05 19:13:19 -0600

    Added the ability to move the drawing, fixed some vertical line bugs, added a frequency slider, and added ability to resize the window

--- CustomizableSine.java
+++ CustomizableSine.java
@@ -27,16 +27,19 @@ public class CustomizableSine extends JFrame implements ActionListener {
     final JButton triangleButton = new JButton();
     final JButton polygonButton = new JButton();
     final JButton drawButton = new JButton();
+    final JLabel rotationLabel = new JLabel();
     final JSlider rotationSlider = new JSlider();
-    final JLabel polygonLabel = new JLabel("Side #:");
+    final JLabel polygonLabel = new JLabel();
     final JSlider polygonSlider = new JSlider();
+    final JLabel frequencyLabel = new JLabel();
+    final JSlider frequencySlider = new JSlider();
 
     public CustomizableSine() {
         super();
         setTitle("Customizable Sine!");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1516,589);
-        setResizable(false);
+        //setResizable(false);
 
         toolBar.setLayout(new FlowLayout(FlowLayout.LEFT));
 
@@ -53,13 +56,23 @@ public class CustomizableSine extends JFrame implements ActionListener {
         polygonSlider.addChangeListener(new ChangeListener() {
             @Override
             public void stateChanged(ChangeEvent e) {
+                //update label (centered and multiline, of course) and create new polygon
+                polygonLabel.setText("<html><center>Sides:<br>" + polygonSlider.getValue() + "</center></html>");
                 actionPerformed(new ActionEvent(this, 0, "polygon"));
             }
         });
         rotationSlider.addChangeListener(new ChangeListener() {
             @Override
             public void stateChanged(ChangeEvent e) {
+                //update label and create new polygon
+                rotationLabel.setText("<html><center>Rotation:<br>" + rotationSlider.getValue() + "%</center></html>");
                 performRotation();
+            }
+        });
+        frequencySlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                frequencyLabel.setText("<html><center>Frequency:<br>"+String.format("%.1f", frequencySlider.getValue()/10.0)+"</center></html>");
             }
         });
 
@@ -75,6 +88,13 @@ public class CustomizableSine extends JFrame implements ActionListener {
         triangleButton.setToolTipText("Triangle");
         polygonButton.setToolTipText("Polygon");
         drawButton.setToolTipText("Free Draw");
+
+        frequencySlider.setMinimum(0);
+        frequencySlider.setMaximum(40);
+        frequencySlider.setPaintTicks(true);
+        frequencySlider.setMinorTickSpacing(5);
+        frequencySlider.setMajorTickSpacing(10);
+        frequencySlider.setValue(10);
 
         rotationSlider.setMinimum(0);
         rotationSlider.setMaximum(100);
@@ -101,7 +121,9 @@ public class CustomizableSine extends JFrame implements ActionListener {
         toolBar.add(polygonButton);
         toolBar.add(drawButton);
         toolBar.add(new JToolBar.Separator());
-        toolBar.add(new Label("Rotation:"));
+        toolBar.add(frequencyLabel);
+        toolBar.add(frequencySlider);
+        toolBar.add(rotationLabel);
         toolBar.add(rotationSlider);
         toolBar.add(polygonLabel);
         toolBar.add(polygonSlider);
@@ -142,8 +164,8 @@ public class CustomizableSine extends JFrame implements ActionListener {
 
             double angle = 0;
             double step = 2*Math.PI/360;
-            double center = 250;
-            double radius = 125;
+            double center = drawPanel.getHeight()/2;
+            double radius = drawPanel.getHeight()/4;
 
             double lastX = center + radius;
             double lastY = center;
@@ -172,11 +194,13 @@ public class CustomizableSine extends JFrame implements ActionListener {
 
             drawPanel.drawingLines.clear();
             drawPanel.waveLines.clear();
-            
-            drawPanel.drawingLines.add(new Line2D.Double(125, 375, 375, 375));
-            drawPanel.drawingLines.add(new Line2D.Double(125, 125, 375, 125));
-            drawPanel.drawingLines.add(new Line2D.Double(375, 125, 375, 375));
-            drawPanel.drawingLines.add(new Line2D.Double(125, 125, 125, 375));
+
+            double center = drawPanel.getHeight()/2;
+
+            drawPanel.drawingLines.add(new Line2D.Double(center/2, center+center/2, center+center/2, center+center/2));
+            drawPanel.drawingLines.add(new Line2D.Double(center/2, center/2, center+center/2, center/2));
+            drawPanel.drawingLines.add(new Line2D.Double(center+center/2, center/2, center+center/2, center+center/2));
+            drawPanel.drawingLines.add(new Line2D.Double(center/2, center/2, center/2, center+center/2));
         }
         else if (command.equals("triangle")) {
             circleButton.setEnabled(true);
@@ -193,9 +217,11 @@ public class CustomizableSine extends JFrame implements ActionListener {
             drawPanel.drawingLines.clear();
             drawPanel.waveLines.clear();
 
-            drawPanel.drawingLines.add(new Line2D.Double(125, 375, 375, 375));
-            drawPanel.drawingLines.add(new Line2D.Double(125, 375, 250, 125));
-            drawPanel.drawingLines.add(new Line2D.Double(250, 125, 375, 375));
+            double center = drawPanel.getHeight()/2;
+
+            drawPanel.drawingLines.add(new Line2D.Double(center/2, center+center/2, center+center/2, center+center/2));
+            drawPanel.drawingLines.add(new Line2D.Double(center/2, center+center/2, center, center/2));
+            drawPanel.drawingLines.add(new Line2D.Double(center, center/2, center+center/2, center+center/2));
         }
         else if (command.equals("polygon")) {
             circleButton.setEnabled(true);
@@ -214,8 +240,8 @@ public class CustomizableSine extends JFrame implements ActionListener {
 
             double angle = 0;
             double step = 2*Math.PI/polygonSlider.getValue();
-            double center = 250;
-            double radius = 125;
+            double center = drawPanel.getHeight()/2;
+            double radius = drawPanel.getHeight()/4;
 
             double lastX = center + radius;
             double lastY = center;
@@ -250,12 +276,13 @@ public class CustomizableSine extends JFrame implements ActionListener {
     public void performRotation() {
         drawPanel.rotatedLines.clear();
         double rotAngle = 2*Math.PI*rotationSlider.getValue()/100;
+        double center = drawPanel.getHeight()/2;
         double x1,y1,x2,y2;
         for (Line2D.Double line : drawPanel.drawingLines) {
-            x1 = (line.x1-250)*Math.cos(rotAngle) - (line.y1-250)*Math.sin(rotAngle) + 250;
-            y1 = (line.x1-250)*Math.sin(rotAngle) + (line.y1-250)*Math.cos(rotAngle) + 250;
-            x2 = (line.x2-250)*Math.cos(rotAngle) - (line.y2-250)*Math.sin(rotAngle) + 250;
-            y2 = (line.x2-250)*Math.sin(rotAngle) + (line.y2-250)*Math.cos(rotAngle) + 250;
+            x1 = (line.x1-center)*Math.cos(rotAngle) - (line.y1-center)*Math.sin(rotAngle) + center;
+            y1 = (line.x1-center)*Math.sin(rotAngle) + (line.y1-center)*Math.cos(rotAngle) + center;
+            x2 = (line.x2-center)*Math.cos(rotAngle) - (line.y2-center)*Math.sin(rotAngle) + center;
+            y2 = (line.x2-center)*Math.sin(rotAngle) + (line.y2-center)*Math.cos(rotAngle) + center;
             drawPanel.rotatedLines.add(new Line2D.Double((int)x1,(int)y1,(int)x2,(int)y2));
         }
     }
@@ -263,11 +290,11 @@ public class CustomizableSine extends JFrame implements ActionListener {
     public class DrawPanel extends JPanel implements MouseMotionListener, MouseListener {
         public ArrayList<Line2D.Double> drawingLines, rotatedLines, waveLines;
         private int lastX, lastY, originalX, originalY;
-        private double angle;
+        private double angle, angleChange;
         private boolean currentlyDrawing = false;
         public int mode; //0: circle, 1: square, 2: triangle, 3: draw
 
-        public static final int DRAW_REGION_SIZE = 500;
+        public int drawRegionSize;
         public static final int waveSpeed = 2;
 
         public DrawPanel() {
@@ -279,17 +306,27 @@ public class CustomizableSine extends JFrame implements ActionListener {
             rotatedLines = new ArrayList<Line2D.Double>();
             waveLines = new ArrayList<Line2D.Double>();
             angle = 0;
+            angleChange = 0.016*2*Math.PI;
             mode = 0;
+            drawRegionSize = getHeight();
+            lastX = -1;
+            lastY = -1;
         }
 
         @Override
+        /**
+         *
+         *  Pretty much all main logic here
+         *
+         */
         public void paint(Graphics g) {
             super.paint(g);
 
             //choose line list to use based on whether or not we chose to rotate our drawing
             ArrayList<Line2D.Double> chosenLines = (rotationSlider.getValue() == 0 ? drawingLines : rotatedLines);
 
-            double center = DRAW_REGION_SIZE/2;
+            drawRegionSize = getHeight();
+            double center = drawRegionSize /2;
 
             //draw all lines of the drawing
             g.setColor(Color.black);
@@ -298,22 +335,32 @@ public class CustomizableSine extends JFrame implements ActionListener {
             }
 
             //draw line to starting point that gets updated every frame
-            if (mode == 4 && currentlyDrawing)
+            if (mode == 4 && currentlyDrawing) {
                 g.drawLine(originalX, originalY, lastX, lastY);
+            }
 
             //draw crosshair
             g.setColor(Color.gray);
-            g.drawLine((int)center-10, (int)center, (int)center+10, (int)center);
-            g.drawLine((int)center, (int)center-10, (int)center, (int)center+10);
+            g.drawLine((int) center - 10, (int) center, (int) center + 10, (int) center);
+            g.drawLine((int) center, (int) center - 10, (int) center, (int) center + 10);
 
-            angle += .016*2*Math.PI;
+            angleChange = .016*2*Math.PI*frequencySlider.getValue()/10;
+            angle += angleChange;
             if (angle >= -2*Math.PI) angle += 2*Math.PI;    //restrict angle to [0, 2*pi)
+
+            //make sure drawing lines don't make their way into the wave region
+            g.setColor(Color.white);
+            g.fillRect(drawRegionSize+1,0,getWidth(),getHeight());
 
             //draw divider and wave x-axis line
             g.setColor(Color.gray);
-            g.drawLine(500, 0, 500, 500);
+            g.drawLine(drawRegionSize, 0, drawRegionSize, drawRegionSize);
             g.setColor(Color.gray.brighter());
-            g.drawLine(501, getHeight()/2, getWidth(), getHeight()/2);
+            g.drawLine(drawRegionSize + 1, getHeight() / 2, getWidth(), getHeight() / 2);
+
+            //givin' myself some props
+            g.setColor(Color.gray.brighter());
+            g.drawString("made by TheGag96", getWidth() - 115, getHeight() - 10);
 
             //if we're drawing or there is no drawing, we don't need to calculate any intersections nor draw the wave
             if (currentlyDrawing || drawingLines.size() == 0) return;
@@ -322,8 +369,8 @@ public class CustomizableSine extends JFrame implements ActionListener {
 
             //negative angle is used in the trig functions because in swing coordinates, increasing y means going down, not up
             Line2D.Double centerLine = new Line2D.Double(center, center,
-                    center+DRAW_REGION_SIZE/2*Math.sqrt(2)*Math.cos(-angle),
-                    center+DRAW_REGION_SIZE/2*Math.sqrt(2)*Math.sin(-angle));
+                    center+ drawRegionSize /2*Math.sqrt(2)*Math.cos(-angle),
+                    center+ drawRegionSize /2*Math.sqrt(2)*Math.sin(-angle));
 
             //find intersection from center to the nearest point on the drawing
             double intersectX = -1, intersectY = -1;
@@ -343,7 +390,7 @@ public class CustomizableSine extends JFrame implements ActionListener {
                         newIntersectY = m1*newIntersectX+b1;
                     }
 
-                    if ((int)line.getX2()-(int)line.getX1() == 0) {
+                    if ((int)line.getX2()-(int)line.getX1() == 0 || m1 > 1000) {  //second condition needed for some really odd slope issues
                         newIntersectX = line.getX1();
                         newIntersectY = m2*newIntersectX+b2;
                     }
@@ -362,8 +409,9 @@ public class CustomizableSine extends JFrame implements ActionListener {
             }
 
             //draw red line from intersection to divider
-            if (intersectX != -1)
-                g.drawLine((int)intersectX, (int)intersectY, 500, (int)intersectY);
+            if (intersectX != -1) {
+                g.drawLine((int) intersectX, (int) intersectY, drawRegionSize, (int) intersectY);
+            }
 
             //move over all wave lines and add new one
             for (int i = 0; i < waveLines.size(); i++) {
@@ -371,37 +419,56 @@ public class CustomizableSine extends JFrame implements ActionListener {
                 line.x1 += waveSpeed;
                 line.x2 += waveSpeed;
                 if (line.x1 > getWidth())   //remove if off-screen
+                {
                     waveLines.remove(i);
+                }
             }
 
             if (waveLines.size() == 0) {
-                waveLines.add(new Line2D.Double(500, intersectY, 500+waveSpeed, intersectY));
+                waveLines.add(new Line2D.Double(drawRegionSize, intersectY, drawRegionSize + waveSpeed, intersectY));
             }
             else {
                 Line2D.Double lastLine = waveLines.get(waveLines.size() - 1);
-                waveLines.add(new Line2D.Double(500, intersectY, lastLine.x1, lastLine.y1));
+                waveLines.add(new Line2D.Double(drawRegionSize, intersectY, lastLine.x1, lastLine.y1));
             }
 
             //draw all wave lines
             g.setColor(Color.red);
             for (Line2D.Double line : waveLines) {
-                g.drawLine((int)line.x1, (int)line.y1, (int)line.x2, (int)line.y2);
+                g.drawLine((int) line.x1, (int) line.y1, (int) line.x2, (int) line.y2);
             }
 
-            //givin' myself some props
-            g.setColor(Color.gray.brighter());
-            g.drawString("made by TheGag96", 1390, 495);
 
         }
 
 
         @Override
         public void mouseDragged(MouseEvent e) {
-            //left click only (special code for mousemotionlistener hooks) and also only within the region
-            if (!SwingUtilities.isLeftMouseButton(e) || e.getX() >= DRAW_REGION_SIZE || e.getX() < 0 || e.getY() < 0 || e.getY() > DRAW_REGION_SIZE || mode != 4) return;
+            //only within the drawing region!
+            if (e.getX() >= drawRegionSize || e.getX() < 0 || e.getY() < 0 || e.getY() > drawRegionSize) return;
 
-            //when the mouse gets dragged, add the line from the last point to this one for our drawing
-            drawingLines.add(new Line2D.Double(lastX, lastY, e.getX(), e.getY()));
+            //left click only (special code for mousemotionlistener hooks)
+            if (SwingUtilities.isLeftMouseButton(e) && mode == 4) {
+                //when the mouse gets dragged, add the line from the last point to this one for our drawing
+                drawingLines.add(new Line2D.Double(lastX, lastY, e.getX(), e.getY()));
+            }
+            else if (SwingUtilities.isRightMouseButton(e) && lastX != -1) {    //right click to drag image!
+                for (Line2D.Double line : drawingLines) {
+                    line.x1 += e.getX() - lastX;
+                    line.y1 += e.getY() - lastY;
+                    line.x2 += e.getX() - lastX;
+                    line.y2 += e.getY() - lastY;
+                }
+
+                for (Line2D.Double line : rotatedLines) {
+                    line.x1 += e.getX() - lastX;
+                    line.y1 += e.getY() - lastY;
+                    line.x2 += e.getX() - lastX;
+                    line.y2 += e.getY() - lastY;
+                }
+            }
+
+            //save last positions for next drag
             lastX = e.getX();
             lastY = e.getY();
         }
@@ -418,31 +485,42 @@ public class CustomizableSine extends JFrame implements ActionListener {
 
         @Override
         public void mousePressed(MouseEvent e) {
-            //left click only!
-            if (e.getButton() != MouseEvent.BUTTON1 || e.getX() >= DRAW_REGION_SIZE || e.getX() < 0 || e.getY() < 0 || e.getY() > DRAW_REGION_SIZE || mode != 4) return;
+            //within the region only!
+            if (e.getX() >= drawRegionSize || e.getX() < 0 || e.getY() < 0 || e.getY() > drawRegionSize) return;
 
-            //clear current drawing, store our starting x and y mouse positions for later
-            drawingLines.clear();
-            rotatedLines.clear();
-            waveLines.clear();
-            lastX = e.getX();
-            lastY = e.getY();
+            //if we're drawing, clear current drawing, store our starting x and y mouse positions for later
+            if (e.getButton() == MouseEvent.BUTTON1 && mode == 4) {
+                drawingLines.clear();
+                rotatedLines.clear();
+                waveLines.clear();
+                currentlyDrawing = true;
+            }
+
             originalX = e.getX();
             originalY = e.getY();
-
-            currentlyDrawing = true;
+            lastX = e.getX();
+            lastY = e.getY();
         }
 
         @Override
         public void mouseReleased(MouseEvent e) {
-            //left click only!
-            if (e.getButton() != MouseEvent.BUTTON1 || mode != 4) return;
+            //handle left clicking in drawing mode
+            if (e.getButton() == MouseEvent.BUTTON1 && mode == 4) {
+                //finish off the drawing with a line back to the starting point from the current one
+                if (e.getX() < drawRegionSize)
+                    drawingLines.add(new Line2D.Double(originalX, originalY, e.getX(), e.getY()));
+                currentlyDrawing = false;
+            }
 
-            //finish off the drawing with a line back to the starting point from the current one
-            if (e.getX() < DRAW_REGION_SIZE)
-                drawingLines.add(new Line2D.Double(originalX, originalY, e.getX(), e.getY()));
+            if (e.getButton() == 3 && rotationSlider.getValue() != 0) {
+                //copy rotatedLines into drawingLines, since this is our "new" drawing to be rotated
+                drawingLines.clear();
+                for (Line2D.Double line : rotatedLines) {
+                    drawingLines.add(new Line2D.Double(line.x1, line.y1, line.x2, line.y2));
+                }
+                System.out.println("zing");
+            }
 
-            currentlyDrawing = false;
             rotationSlider.setValue(0);
         }
 
